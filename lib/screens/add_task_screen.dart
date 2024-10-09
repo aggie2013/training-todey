9d3470db5dce2e7eb1f7a704e7d9c770e290415f
Kai He <hekai_aggie2013@163.com>
2024-10-09 09:51:11 +0800

    add bottom sheet widget

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.lightBlueAccent,
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 import 'package:todoey_flutter/widgets/task_list.dart';
 
 class TaskScreen extends StatelessWidget {
@@ -9,7 +10,10 @@ class TaskScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (context) => AddTaskScreen());
+        },
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
